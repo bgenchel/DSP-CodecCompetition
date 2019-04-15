9e1f64db54c9aa9fa2846325ceab0eed8b82403a
bgenchel <benjiegenchel@gmail.com>
2019-04-15 13:10:53 -0400

    separated baseline out into separate function files

---
+++ baseline/midtread_dequantizer.m
@@ -0,0 +1,13 @@
+function [ret_value] = midtread_dequantizer(x, R)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   MIDTREAD_DEQUANTIZER    %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+sign = (2 * (x < 2^(R-1))) - 1;
+Q = 2 / (2^R - 1); 
+
+x_uint = uint32(x);
+x = bitset(x_uint,R,0);      
+x = double(x);
+
+ret_value = sign * Q .* x;
+end
---
+++ baseline/midtread_quantizer.m
@@ -0,0 +1,9 @@
+function [ret_value] = midtread_quantizer(x, R)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%    MIDTREAD_QUANTIZER     %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Q = 2 / (2^R - 1);      
+q = quant(x,Q);
+s = q<0;    
+ret_value = uint16(abs(q)./Q + s*2^(R-1));
+end
